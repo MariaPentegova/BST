ab265f6746249acd689d1016aa15965c6977f2c8
MariaPentegova <m.pentegova@yandex.ru>
2026-04-28 13:37:51 +0300

    Update bst.c

--- src/bst.c
+++ src/bst.c
@@ -1,1 +1,108 @@
+#include <errno.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "bst.h"
 
+static BSTNode* bstNodeCreate(int value)
+{
+    BSTNode* node = (BSTNode*)malloc(sizeof(BSTNode));
+    if (node == NULL) {
+        return NULL;
+    }
+
+    node->value = value;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+BST* bstCreate(void)
+{
+    BST* tree = (BST*)malloc(sizeof(BST));
+    if (tree == NULL) {
+        return NULL;
+    }
+
+    tree->root = NULL;
+    tree->size = 0;
+    return tree;
+}
+
+static void bstNodeFreeRecursive(BSTNode* node)
+{
+    if (node == NULL) {
+        return;
+    }
+
+    bstNodeFreeRecursive(node->left);
+    bstNodeFreeRecursive(node->right);
+    free(node);
+}
+
+void bstFree(BST* tree)
+{
+    if (tree == NULL) {
+        return;
+    }
+
+    bstNodeFreeRecursive(tree->root);
+    free(tree);
+}
+
+size_t bstSize(BST* tree)
+{
+    return tree == NULL ? 0 : tree->size;
+}
+
+bool bstContains(BST* tree, int value)
+{
+    if (tree == NULL) {
+        return false;
+    }
+
+    BSTNode* cur = tree->root;
+    while (cur != NULL) {
+        if (value < cur->value) {
+            cur = cur->left;
+        } else if (value > cur->value) {
+            cur = cur->right;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool bstInsert(BST* tree, int value)
+{
+    if (tree == NULL) {
+        return false;
+    }
+
+    BSTNode** cur = &tree->root;
+    while (*cur != NULL) {
+        if (value < (*cur)->value) {
+            cur = &(*cur)->left;
+        } else if (value > (*cur)->value) {
+            cur = &(*cur)->right;
+        } else {
+            return false;
+        }
+    }
+
+    *cur = bstNodeCreate(value);
+    if (*cur == NULL) {
+        errno = ENOMEM;
+        return false;
+    }
+
+    tree->size++;
+    return true;
+}
+
+int main(){
+  return 0;
+}
