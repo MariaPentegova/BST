0a3b6366982d61b00cc317b9100a02a5a765ec36
MariaPentegova <m.pentegova@yandex.ru>
2026-04-28 13:38:25 +0300

    Create bst.h

---
+++ src/bst.h
@@ -0,0 +1,34 @@
+#ifndef BST_H
+#define BST_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef struct BSTNode {
+    int value;
+    struct BSTNode* left;
+    struct BSTNode* right;
+} BSTNode;
+
+typedef struct BST {
+    BSTNode* root;
+    size_t size;
+} BST;
+
+typedef struct Iterator {
+    BSTNode** stack;
+    size_t top;
+    size_t capacity;
+    bool failed;
+} Iterator;
+
+BST* bstCreate(void);
+void bstFree(BST* tree);
+
+/* true если вставили false если уже был или не хватило памяти */
+bool bstInsert(BST* tree, int value);
+
+bool bstContains(BST* tree, int value);
+size_t bstSize(BST* tree);
+
+#endif
